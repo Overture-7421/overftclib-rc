e7c19832576c2ad79a5b2c73bf5e402a7baefc79
SantiQ <a01571222@itesm.mx>
2024-10-21 19:00:45 -0600

    Working with RC

--- src/main/java/org/overture/ftc/OverDcMotorRC.java
+++ src/main/java/org/overture/ftc/OverDcMotorRC.java
@@ -15,7 +15,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import org.overture.ftc.overftclib.IOverDcMotor;
+import com.overture.ftc.overftclib.IOverDcMotor;
 
 public class OverDcMotorRC implements IOverDcMotor {
 
